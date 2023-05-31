47987ed2ac256abf7bfdbb3215f025aebbb029b1
Ruslan <goloruslan64@gmail.com>
2023-05-31 11:28:07 +0300

    add todo app

---
+++ src/routes/project/apps/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="container">
+    <a href="/project">Go back to project</a>
+    <slot></slot>
+</div>
+
+<style>
+.container{
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    } 
+
+</style>
